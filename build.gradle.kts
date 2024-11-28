281b7397ecdb9d1171bf395e84f42d6746b9bbd6
Rolando Santamaria Maso <kyberneees@gmail.com>
2024-11-28 21:52:37 +0100

    adding github CI/CD integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
+import java.io.ByteArrayOutputStream
+
 plugins {
     id("java")
+    id("maven-publish")
 }
-
-group = "de.21no"
-version = "1.0.0-alpha"
 
 repositories {
     mavenCentral()
@@ -26,9 +26,65 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     // https://mvnrepository.com/artifact/org.mockito/mockito-core
     testImplementation("org.mockito:mockito-core:5.14.2")
-
 }
-
 tasks.test {
     useJUnitPlatform()
+}
+
+// Dynamically set the version from the latest Git tag
+version = run {
+    // Function to check if Git is available
+    fun isGitAvailable(): Boolean {
+        return try {
+            exec {
+                commandLine("git", "--version")
+                standardOutput = ByteArrayOutputStream()
+                errorOutput = ByteArrayOutputStream()
+                isIgnoreExitValue = true
+            }.exitValue == 0
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    if (isGitAvailable()) {
+        try {
+            val stdout = ByteArrayOutputStream()
+            exec {
+                commandLine("git", "describe", "--tags", "--abbrev=0")
+                standardOutput = stdout
+                errorOutput = ByteArrayOutputStream()
+                isIgnoreExitValue = true
+            }
+            val gitTag = stdout.toString().trim()
+            gitTag.ifEmpty {
+                "1.0.0-SNAPSHOT" // Default version if no tag is found
+            }
+        } catch (e: Exception) {
+            "1.0.0-SNAPSHOT" // Default version if Git command fails
+        }
+    } else {
+        "1.0.0-SNAPSHOT" // Default version if Git is not available
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components["java"])
+            groupId = "de.n21no.realtime.pubsub"
+            artifactId = "core"
+            version = version
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/BackendStack21/realtime-pubsub-client-java")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
